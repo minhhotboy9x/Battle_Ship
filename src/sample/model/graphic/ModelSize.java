8046944ff025398ececdf56b2fc49e177b8692dd
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2022-06-18 09:57:57 +0700

    hhh

--- src/sample/model/graphic/ModelSize.java
+++ src/sample/model/graphic/ModelSize.java
@@ -4,5 +4,4 @@ public class ModelSize {
     public static int mapSpots = 10; //10x10
     public static double mapCellSize = 20; //10x10
     public static double mapLength = 400; // chieu dai canh map
-
 }
