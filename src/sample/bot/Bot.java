0649480d042e57f4bd24687cccf9348307d9d56c
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2022-06-18 20:12:34 +0700

    'lll'

--- src/sample/bot/Bot.java
+++ src/sample/bot/Bot.java
@@ -5,4 +5,5 @@ import sample.model.graphic.ModelSpec;
 public class Bot {
     public static int mode = 0; //che do 1: de, 2: kho
     public static int map[][] = new int[ModelSpec.mapSpots][ModelSpec.mapSpots]; //get info of map
+
 }
