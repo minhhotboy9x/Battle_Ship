782cb727ddbcfa7aba71b8c149a61f1052b86445
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2022-06-19 15:08:15 +0700

    'l'

--- src/sample/ShipLineup.java
+++ src/sample/ShipLineup.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 import sample.model.Map;
 import static sample.Intro.soundButton;
 import static sample.Intro.soundButtonClick;
+
+import sample.model.Ship;
 import sample.model.graphic.ModelSpec;
 
 public class ShipLineup extends Application {
@@ -17,21 +19,31 @@ public class ShipLineup extends Application {
         Pane root = new Pane();
         Scene scene = new Scene(root, 1280, 720, false, SceneAntialiasing.BALANCED);
         scene.getStylesheets().add("sample/css/style.css");
+
         // vẽ map
         Map lineupMap = new Map(ModelSpec.posLineUpMapX, ModelSpec.posLineUpMapY, root, ModelSpec.lineupMapSize, ModelSpec.mapSpots);
         lineupMap.drawMap();
-        //--------------
+        //----------------------------
 
         // vẽ ship container
         Rectangle rect = new Rectangle();
         rect.setStyle("-fx-fill: rgba(255,255,255,0.5);");
         rect.setHeight(ModelSpec.rectHeight);
         rect.setWidth(ModelSpec.rectWidth);
-
+        //rect.setOnMouseClicked(e->System.out.println("1"));
+        rect.setViewOrder(2.0);
         rect.setStroke(Color.BLACK);
-        rect.setTranslateX(ModelSpec.posLineUpRectX);
-        rect.setTranslateY(ModelSpec.posLineUpRectY);
-        //
+        rect.setTranslateX(ModelSpec.posLineUpRectX); //750
+        rect.setTranslateY(ModelSpec.posLineUpRectY); //80
+        //----------------------------
+
+        //draw ship on container
+        Ship ship1 = new Ship(800, 100, 5, 1, ModelSpec.lineupMapSquareSize, lineupMap, root);
+        Ship ship2 = new Ship(800, 400, 4, 1, ModelSpec.lineupMapSquareSize, lineupMap, root);
+        Ship ship3 = new Ship(950, 200, 3, 1, ModelSpec.lineupMapSquareSize, lineupMap, root);
+        Ship ship4 = new Ship(1000, 500, 3, 1, ModelSpec.lineupMapSquareSize, lineupMap, root);
+        Ship ship5 = new Ship(1050, 350, 2, 1, ModelSpec.lineupMapSquareSize, lineupMap, root);
+        //------------------------------
 
         // soundButton
         // set lai thuoc tinh de debug
@@ -41,6 +53,8 @@ public class ShipLineup extends Application {
         soundButton.setOnAction(e -> {
             soundButtonClick();
         });
+        //-----------------------------
+        primaryStage.setResizable(false);
         root.getChildren().addAll(soundButton, rect);
         primaryStage.setScene(scene);
         primaryStage.show();
--- src/sample/model/graphic/ModelSpec.java
+++ src/sample/model/graphic/ModelSpec.java
@@ -2,22 +2,24 @@ package sample.model.graphic;
 
 public class ModelSpec {
     // map
-    public static int mapSpots = 10; //10x10
-    public static double mapCellSize = 20; //10x10
+    public static int mapSpots = 10; //10x10 for all map
+    //______________
+    public static double mapSquareSize = 20; //chieu dai 1 o
     public static double mapSize = 400; // chieu dai canh map
     //--------------
 
     // lineupMap
     public static double lineupMapSize = 500; //chieu dai lineupMap
-    public static double posLineUpMapX = 80; //vi tri linupMap
+    public static double posLineUpMapX = 50; //vi tri lineupMap
     public static double posLineUpMapY = 150; //vi tri lineupMap
+    public static double lineupMapSquareSize = lineupMapSize / mapSpots; //50
     //--------------
 
     // container lineupShip (chua trong rectangle)
     public static double rectWidth = 400; //chieu rong lineupMap
     public static double rectHeight = 600; //chieu dai lineupMap
 
-    public static double posLineUpRectX = 750; //vi tri linupMap
+    public static double posLineUpRectX = 770; //vi tri linupMap
     public static double posLineUpRectY = 80; //vi tri lineupMap
     //
 }
