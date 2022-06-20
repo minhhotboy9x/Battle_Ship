2c9274aefe937bbc72784eda73837282a1cdc37f
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2022-06-21 02:34:33 +0700

    bot upd

--- src/sample/bot/Bot.java
+++ src/sample/bot/Bot.java
@@ -2,9 +2,11 @@ package sample.bot;
 
 import sample.model.graphic.ModelSpec;
 
+import java.util.ArrayList;
+
 public class Bot {
     public static int mode = 0; //che do 1: de, 2: kho
     public static int map[][] = new int[ModelSpec.mapSpots][ModelSpec.mapSpots]; //get info of map
     // 0 chua ban, 1 ban hut, 2 ban trung, 3 thuyen bi ha
-
+    ArrayList<Integer> playShipList;
 }
