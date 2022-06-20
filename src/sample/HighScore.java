75bcab20777ddede1d485508c921f07a04089b10
loi vu <loi07062001@gmail.com>
2022-06-20 23:25:10 +0700

    giao dien dau intro diem so sua giao dien cho che do kho de them nut returnHighscore

--- src/sample/HighScore.java
+++ src/sample/HighScore.java
@@ -3,9 +3,12 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -33,18 +36,46 @@ public class HighScore extends Application {
         table.setEditable(true);
         TableColumn nameCol = new TableColumn("Name");
         nameCol.setMinWidth(250);
+        nameCol.setMaxWidth(250);
         TableColumn scoreCol = new TableColumn("Score");
         scoreCol.setMinWidth(120);
+        scoreCol.setMaxWidth(120);
         TableColumn rankCol = new TableColumn("Ranking");
         rankCol.setMinWidth(120);
+        rankCol.setMaxWidth(120);
         table.getColumns().addAll(rankCol , nameCol, scoreCol);
+
 
         table.setLayoutX(400);
         table.setLayoutY(200);
-        table.setMinWidth(500);
+        table.setMinWidth(490);
+
+        //set return button
+        Image returnImage = new Image("resource/image/button/mute.png");
+        ImageView returnImageView = new ImageView(returnImage);
+
+        returnImageView.setFitWidth(50);
+        returnImageView.setFitHeight(50);
 
 
-        root.getChildren().addAll(label, table);
+
+        Button returnButton = new Button("", returnImageView);
+        returnButton.setLayoutX(320);
+        returnButton.setLayoutY(200);
+        returnButton.setOnAction(e->{
+            //change scene
+            Intro introScene = new Intro();
+            try {
+
+                introScene.start(stage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+
+
+
+        root.getChildren().addAll(label, table, returnButton);
 
         stage.setResizable(false);
         stage.sizeToScene();
